dcab52e9d02008ae8a0ce74f04b7a39537e00279
Marty <marty@martyweb.com>
2023-09-04 14:39:01 -0500

    file cleanup

---
+++ cleanup_baddates.ps1
@@ -0,0 +1,114 @@
+﻿#Find pictures with bad dates ie. from the future and remove them
+
+#Variables
+param (
+  [Parameter(Mandatory = $true)]$directory, 
+  $dirfilter = "*",
+  $dryrun = 1,
+  $doit="NO",
+  $recurisve=0)
+
+#validate with user that they want to continue
+Write-Output("Reading directory $directory with a filter of $dirfilter")
+$doit = read-host -Prompt "Continue? type YES"
+if($doit -ne "YES"){exit}
+
+
+#Main program function
+Function Organize-Photos { 
+  Param(
+    [Parameter(Mandatory = $true)][string]$basedirectory, 
+    [string]$dirfilter
+  )
+  
+  if($recurisve){
+    Write-Output "Recursive"
+    $folders = Get-ChildItem -Path $basedirectory -Directory
+  }else{
+    Write-Output "Not recursive"
+    $folders = Get-Item -Path $basedirectory
+  }
+  Write-Output "Reading $folders"
+  
+  #loop through sub folders
+  foreach ($sFolder in $folders) {
+
+    #make dir path with filter
+    $sFolderFullPath = $sFolder.FullName + "\"
+    $dirWithFilter = $sFolderFullPath + $dirfilter
+    Write-Output "Reading $dirWithFilter"
+   
+    #loop through files in folder
+    foreach ($File in Get-ChildItem -Path $dirWithFilter -Exclude "*.json*" -File) { 
+ 
+      Write-Output($File.FullName)
+
+      #get metadata from file using exiftool
+      $data = & ./exiftool.exe $File.FullName -j -q -q -overwrite_original
+      $obj = $data | ConvertFrom-Json
+      Write-Output($obj)
+
+      #get date from exif output
+      $sDateTaken = ""
+      if ($obj.CreateDate) { 
+        $sDateTaken = $obj.CreateDate
+        $aDateTaken = $sDateTaken.split(" ")
+        $sNewFolderNameCreateDate = $aDateTaken[0] -replace ":","-" 
+      }
+      if ($obj.DateTimeOriginal) { 
+        $sDateTaken = $obj.DateTimeOriginal
+        $aDateTaken = $sDateTaken.split(" ")
+        $sNewFolderName = $aDateTaken[0] -replace ":","-"
+      }
+      if([string]$sNewFolderName -isnot [DateTime]){
+        if([string]$sNewFolderNameCreateDate -as [DateTime]){
+          $sNewFolderName=$sNewFolderNameCreateDate
+        }else {
+          $sNewFolderName=""
+        }
+      }
+      
+      #Write-Output($sNewFolderName)
+      #exit
+      
+
+      if($sNewFolderName -eq ""){
+        Write-Output("No new folder, not moved")
+      }else{
+        $des_path = $sFolderFullPath + $sNewFolderName
+        #Write-Output($des_path)
+        #exit
+        if (!$dryrun) {
+          Move-File $File.FullName $des_path
+        }else{
+          Write-Output("Dryrun, not moved to " + $des_path)
+        }
+        #exit
+    
+      }
+      
+            
+    }
+  }
+}
+
+#moves file into directory, creates if doesn't exist
+Function Move-File {
+  Param(
+    [Parameter(Mandatory = $true)][string] $File,
+    [Parameter(Mandatory = $true)][string] $des_path
+  ) 
+  #check if dest dir exists
+  if (test-path $des_path) { 
+    move-item $File $des_path 
+  }
+  else {
+    new-item -ItemType directory -Path $des_path.Trim() #create directory
+    move-item $File $des_path 
+  }
+  Write-Output("Moved " + $File + " to " + $des_path)
+}
+
+
+#run the program
+Organize-Photos $directory $dirfilter
---
+++ set date on photos.ps1
@@ -0,0 +1,117 @@
+﻿#loop through folder and set the year on pictures to match folder
+
+#Variables
+param (
+  [Parameter(Mandatory = $true)]$directory, 
+  $dirfilter = "*",
+  $dryrun = 0,
+  $recurisve=0)
+
+#validate with user that they want to continue
+Write-Output("Reading directory $directory with a filter of $dirfilter")
+
+#Main program function
+Function Organize-Photos { 
+  Param(
+    [Parameter(Mandatory = $true)][string]$basedirectory, 
+    [string]$dirfilter
+  )
+  
+  if($recurisve){
+    Write-Output "Recursive"
+    $folders = Get-ChildItem -Path $basedirectory -Directory -Filter  "*"
+  }else{
+    Write-Output "Not recursive"
+    $folders = Get-Item -Path $basedirectory
+  }
+  #Write-Output "Reading $folders"
+  
+  #loop through sub folders
+  foreach ($sFolder in $folders) {
+
+    $folderYear = ($sFolder.Name).substring(0,4)
+    $folderDesc = $sFolder.Name
+    #Write-Output($folderDesc)
+    #exit
+    
+    #make dir path with filter
+    $sFolderFullPath = $sFolder.FullName + "\"
+    $dirWithFilter = $sFolderFullPath + $dirfilter
+    Write-Output "Reading $dirWithFilter"
+   
+    #loop through files in folder
+    foreach ($File in Get-ChildItem -Path $dirWithFilter -Exclude "*.json*" -File) { 
+ 
+      Write-Output("Reading " + $File.FullName)
+
+      #get metadata from file using exiftool
+      $data = & ./exiftool.exe $File.FullName -j -q -q -overwrite_original
+      $obj = $data | ConvertFrom-Json
+      #Write-Output("Original datetime: " + $obj.DateTimeOriginal)
+      #Write-Output($obj)
+
+      #get pic year from metadata
+      if ($obj.DateTimeOriginal){
+        $picYear = $obj.DateTimeOriginal.substring(0,4)
+      }else{
+        $picYear=$folderYear
+      }
+      $dCreatedDateTime = $folderYear + "-01-01 00:00:00"
+      $dCreatedDate = $folderYear + "-01-01"
+
+      #set DateCreated
+      if($obj.DateCreated){
+        #get year
+        #$DateCreatedobj = [DateTime]$obj.DateCreated
+        $DateCreatedYear = $obj.DateCreated.substring(0,4)
+        #Write-Output($DateCreatedYear + $folderYear + "-------")
+        #does it match folder
+        if($DateCreatedYear -ne $folderYear){
+          #dry run test
+          if (!$dryrun) {
+            & ./exiftool "-overwrite_original" "-Description=`"$folderDesc`"" "-CreateDate=`"$dCreatedDateTime`"" "-DateCreated=`"$dCreatedDate`"" "-YearCreated=`"$folderYear`"" $File.FullName -q
+            Write-Output($DateCreatedYear)
+            Write-Output("Writing DateCreated " + $dCreatedDate + " to file " + $File.FullName)
+          }else{
+            Write-Output("DRYRUN: Would have Written DateCreated " + $dCreatedDate + " to file " + $File.FullName)
+          }
+        }
+      }
+
+      #set date if no in exif data or mismatch year
+      $sDateTaken = ""
+      if ((-not $obj.DateTimeOriginal) -or ($folderYear -ne $picYear)) { 
+
+        if ($folderYear -ne $picYear -and $picYear -ne "") { 
+          $dCreatedDateTime = $folderYear + $obj.DateTimeOriginal.substring(4, 6).replace(":","-")
+          Write-Output("Year mismatch: " + $obj.DateTimeOriginal + " should be: " + $dCreatedDateTime)
+          #Write-Output("-------------")
+          
+        }else{
+          Write-Output("No datetime set")
+        }
+
+        #write metadata
+        
+        #Write-Output($dCreatedDate)
+        #-overwrite_original
+        #Write-Output("-Description=`"$folderDesc`" -DateTimeOriginal=`"$(Get-Date $dCreatedDate -format "yyyy:MM:dd") 00:00:00 `"")
+        #exit
+        if (!$dryrun) {
+          & ./exiftool "-overwrite_original" "-Description=`"$folderDesc`"" "-DateTimeOriginal=$(Get-Date $dCreatedDateTime -format "yyyy:MM:dd") 00:00:00" $File.FullName -q
+          Write-Output("Writing DateTimeOriginal " + $dCreatedDateTime + " to file " + $File.FullName)
+        }else{
+          Write-Output("DRYRUN: Would have Written DateTimeOriginal " + $dCreatedDateTime + " to file " + $File.FullName)
+        }
+        
+        #exit
+      }else{
+        #Write-Output("Did not update date: " + $obj.DateTimeOriginal)
+      }
+             
+    }
+  }
+}
+
+#run the program
+Organize-Photos $directory $dirfilter
